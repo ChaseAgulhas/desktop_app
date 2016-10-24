4c7eb26f03bf95ccbd2d41f96a9f6e6e7efafb63
Ijaaz <ijaazlagardien@gmail.com>
2016-10-24 15:26:10 +0200

    Intial commit, started working on domain

---
+++ DesktopApp/main/domain/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace DesktopApp.main.domain
+{
+    [Serializable]
+    class Menu
+    {
+        private string category;
+        public Menu()
+        {
+
+        }
+
+        public void setCategory(string category)
+        {
+            this.category = category;
+        }
+
+        public string getCategory()
+        {
+            return category;
+        }
+    }
+}
