b7d5aee470f92fe681b2031a59af721fcb21265f
Ijaaz <ijaazlagardien@gmail.com>
2016-10-24 17:38:06 +0200

    Finished Foodtem class

--- DesktopApp/main/domain/FoodItem.cs
+++ DesktopApp/main/domain/FoodItem.cs
@@ -1,12 +1,86 @@
-﻿using System;
+﻿/*
+    Author: Ijaaz Lagardien
+    Student Number: 214167542
+    File: Address.cs
+    Subject: Technical Programming 2
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace DesktopApp.main.domain
 {
-    class FoodItem
+    [Serializable]
+    class FoodItem : ISerializable
     {
+        private String name;
+        private double price;
+        private int amountAvailable;
+
+        public FoodItem(){}
+
+        public string Name
+        {
+            get{return name;}
+            set{name = value;}
+        }
+
+        public double Price
+        {
+            get{return price;}
+            set{price = value;}
+        }
+
+        public int AmountAvailable
+        {
+            get{return amountAvailable;}
+            set{amountAvailable = value;}
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public class Builder
+        {
+            private String name;
+            private double price;
+            private int amountAvailable;
+
+            public Builder() { }
+
+            public Builder Name(String name)
+            {
+                this.name = name;
+                return this;
+            }
+
+            public Builder Price(double price)
+            {
+                this.price = price;
+                return this;
+            }
+
+            public Builder AmountAvailable(int amountAvailable)
+            {
+                this.amountAvailable = amountAvailable;
+                return this;
+            }
+
+            public Builder copy(FoodItem foodItem)
+            {
+                this.name = foodItem.name;
+                this.price = foodItem.price;
+                this.amountAvailable = foodItem.amountAvailable;
+
+                return this;
+            }
+
+            public FoodItem build() { return new FoodItem(); }
+        }
     }
 }
