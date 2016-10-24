f9d5869b5801b7ee636bc95c49cb1f2a742fc3d5
Ijaaz <ijaazlagardien@gmail.com>
2016-10-24 18:23:12 +0200

    Updated DeliveryImpl class

--- DesktopApp/main/domain/delivery/impl/DeliveryImpl.cs
+++ DesktopApp/main/domain/delivery/impl/DeliveryImpl.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+    Author: Ijaaz Lagardien
+    Student Number: 214167542
+    File: Delivery.cs
+    Subject: Technical Programming 2
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
