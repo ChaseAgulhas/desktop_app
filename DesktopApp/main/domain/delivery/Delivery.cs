0d40329a3f2e002a1f53f10b994b9d3235fe426f
Ijaaz <ijaazlagardien@gmail.com>
2016-10-24 18:04:30 +0200

    Finished Delivery interface

---
+++ DesktopApp/main/domain/delivery/Delivery.cs
@@ -0,0 +1,21 @@
+﻿/*
+    Author: Ijaaz Lagardien
+    Student Number: 214167542
+    File: Address.cs
+    Subject: Technical Programming 2
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace DesktopApp.main.domain
+{
+
+    interface Delivery
+    {
+        double calculateTotalCost(double totalDistance);
+    }
+}
