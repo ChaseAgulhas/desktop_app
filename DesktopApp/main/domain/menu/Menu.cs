ad61afa5d83eb5e29bb6af92b536d674c2f4e9c5
Ijaaz Lagardien <ijaazlagardien@gmail.com>
2016-11-19 17:12:17 +0200

    Updated Menu Class and completed MenuFactory

--- DesktopApp/main/domain/menu/Menu.cs
+++ DesktopApp/main/domain/menu/Menu.cs
@@ -5,11 +5,6 @@
     Subject: Technical Programming 2
 */
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime.Serialization;
 
 namespace DesktopApp.main.domain
 {
@@ -23,30 +18,6 @@ namespace DesktopApp.main.domain
         {
             get{return category;}
             set{ category = value; }
-        }
-
-        private class Builder
-        {
-            private String category;
-
-            public Builder() { }
-
-            public Builder Category(String category)
-            {
-                this.category = category;
-                return this;
-            }
-
-            public Builder copy(Menu menu)
-            {
-                this.category = menu.category;
-                return this;
-            }
-
-            public Menu build()
-            {
-                return new Menu();
-            }
         }
     }
 }
--- DesktopApp/main/factories/menu/MenuFactory.cs
+++ DesktopApp/main/factories/menu/MenuFactory.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+    Author: Ijaaz Lagardien
+    Student Number: 214167542
+    File: MenuFactory.cs
+    Subject: Technical Programming 2
+*/
+using DesktopApp.main.domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +15,11 @@ namespace DesktopApp.main.factories.menu
 {
     class MenuFactory
     {
+        public static Menu getMenu(String category)
+        {
+            Menu menuObject = new Menu();
+            menuObject.Category = category;
+            return menuObject;
+        }
     }
 }
