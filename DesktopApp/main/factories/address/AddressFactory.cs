b7a15a12ca727785f792133c96a9dabbdb57f10c
Ijaaz Lagardien <ijaazlagardien@gmail.com>
2016-11-19 16:25:50 +0200

    Updated Address domain class and completed AddressFactory class

--- DesktopApp/main/factories/address/AddressFactory.cs
+++ DesktopApp/main/factories/address/AddressFactory.cs
@@ -8,5 +8,15 @@ namespace DesktopApp.main.factories.address
 {
     class AddressFactory
     {
+        public static Address getAddress(String streetNumber, String streetName, String suburb, String city, String postCode)
+        {
+            Address address = new Address();
+            address.StreetNumber = streetNumber;
+            address.StreetNumber = streetNumber;
+            address.Suburb = suburb;
+            address.City = city;
+            address.PostCode = postCode;
+            return address; 
+        }
     }
 }
