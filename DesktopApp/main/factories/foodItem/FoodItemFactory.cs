6a4c609bd486c0048e3ec38a552626e4a6028476
Ijaaz Lagardien <ijaazlagardien@gmail.com>
2016-11-19 16:11:58 +0200

    Added Factories

---
+++ DesktopApp/main/factories/foodItem/FoodItemFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopApp.main.factories.foodItem
+{
+    class FoodItemFactory
+    {
+    }
+}
