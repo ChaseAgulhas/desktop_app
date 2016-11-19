c50cf22c03d8675280a4b5d8bb5903544955bb61
Ijaaz Lagardien <ijaazlagardien@gmail.com>
2016-11-19 17:08:31 +0200

    Edited Login class and completed LoginFactory

--- DesktopApp/main/domain/login/Login.cs
+++ DesktopApp/main/domain/login/Login.cs
@@ -5,10 +5,6 @@
     Subject: Technical Programming 2
 */
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Runtime.Serialization;
 
 namespace DesktopApp.main.domain
@@ -39,33 +35,5 @@ namespace DesktopApp.main.domain
             throw new NotImplementedException();
         }
 
-        private class Builder
-        {
-            private String username;
-            private String password;
-
-            public Builder() { }
-
-            public Builder Username(String username)
-            {
-                this.username = username;
-                return this;
-            }
-
-            public Builder Password(String password)
-            {
-                this.password = password;
-                return this;
-            }
-
-            public Builder copy(Login login)
-            {
-                this.username = login.username;
-                this.password = login.password;
-                return this;
-            }
-
-            public Login build() { return new Login(); }
-        }
     }
 }
--- DesktopApp/main/factories/login/LoginFactory.cs
+++ DesktopApp/main/factories/login/LoginFactory.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+    Author: Ijaaz Lagardien
+    Student Number: 214167542
+    File: LoginFactory.cs
+    Subject: Technical Programming 2
+*/
+using DesktopApp.main.domain;
+using System;
 
 namespace DesktopApp.main.factories.login
 {
     class LoginFactory
     {
+        public static Login getLogin(String username,String password)
+        {
+            Login loginObject = new Login();
+            loginObject.Username = username;
+            loginObject.Password = password;
+            return loginObject;
+        }
     }
 }
