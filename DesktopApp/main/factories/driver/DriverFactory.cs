758851decd60662a83403a4616fa595bed79b767
Ijaaz Lagardien <ijaazlagardien@gmail.com>
2016-11-19 16:55:04 +0200

    Competed DriverFactory and updated Driver class

--- DesktopApp/main/factories/driver/DriverFactory.cs
+++ DesktopApp/main/factories/driver/DriverFactory.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+    Author: Ijaaz Lagardien
+    Student Number: 214167542
+    File: DriverFactory.cs
+    Subject: Technical Programming 2
+*/
+using DesktopApp.main.domain;
+using System;
 
 namespace DesktopApp.main.factories.driver
 {
     class DriverFactory
     {
+        public static Driver getDriver(String name, String surname,String cellphoneNumber,String email,String password, byte[] profilePhoto)
+        {
+            Driver driverObject = new Driver();
+            driverObject.Name = name;
+            driverObject.Surname = surname;
+            driverObject.CellPhoneNumber = cellphoneNumber;
+            driverObject.Email = email;
+            driverObject.Password = password;
+            driverObject.ProfilePhoto = profilePhoto;
+            return driverObject;
+        }
     }
 }
